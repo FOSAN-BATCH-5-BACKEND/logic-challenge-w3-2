550cc9f99798dbfd6a4bf5c33e757bc063957bea
Rijal Fadhila <r3dlabelz@gmail.com>
2021-05-06 20:30:30 +0700

    semangat mengerjakan

---
+++ halteBis.js
@@ -0,0 +1,12 @@
+/* Anda adalah seorang sopir bus. Anda menurunkan dan menerima penumpang pada setiap halte.Di asumsikan dalam sebuah array, berisi beberapa halte, dan pada setiap halte berisi 2 angka, angka yang pertama adalah jumlah penumpang masuk, angka kedua adalah jumlah penumpang yang turun. buatlah sebuah fungsi untuk menghitung sisa penumpang yang ada di dalam bus.
+*/
+
+let jumlah = function(halte){
+    // Good Luck!
+  }
+
+
+
+console.log(jumlah([[10,0],[3,5],[5,8]])) //5
+console.log(jumlah([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])) //17
+console.log(jumlah([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]])) //21
